bd581c2dc9c991753ce2795e77a526ee69f81f04
YaelMedinaRodriguez <72525117+YaelMedinaRodriguez@users.noreply.github.com>
2020-10-07 16:18:04 -0500

    Create Practica_1.c

---
+++ Practica_1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+  int main()
+{
+  printf("este es mi segundo codigo\n");
+  printf("programar es facil\n");
+  printf("voy a compartir mi codigo con un compañero\n");
+  return 0;
+}
