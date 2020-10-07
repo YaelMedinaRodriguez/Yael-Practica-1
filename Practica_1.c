7ab50d16e26965d39e45a55cdb34dec316534c32
Diego García <72525046+DiegoGHz@users.noreply.github.com>
2020-10-07 16:27:27 -0500

    Agregue mi nombre con printf

--- Practica_1.c
+++ Practica_1.c
@@ -4,5 +4,6 @@
   printf("este es mi segundo codigo\n");
   printf("programar es facil\n");
   printf("voy a compartir mi codigo con un compañero\n");
+  printf("\nDiego Garcia Hernandez\n");
   return 0;
 }
